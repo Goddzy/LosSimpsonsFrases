b9a6da8d7e62103b311047361d35ec3e06a809b2
Goddzy <renatotrevisiolmontiel@gmail.com>
2023-08-17 14:37:40 -0300

    los datos se ven en el cardPersonaje

--- src/components/CardPersonaje.jsx
+++ src/components/CardPersonaje.jsx
@@ -1,31 +1,24 @@
 import React from "react";
-import { Card, Row, Col } from "react-bootstrap";
-const CardPersonaje = ({personaje}) => {
+import { Card, Row, Col, Container } from "react-bootstrap";
+const CardPersonaje = ({ personaje }) => {
   return (
-    <Card className="mt-5 rounded-4 w-100">
-      <Row>
-        <Col md={4}>
-          <Card.Img
-            variant="left"
-            className="w-100"
-            height={700}
-            src="https://trello.com/1/cards/6477b8546c9b62d74f338f39/attachments/6477b8546c9b62d74f338f99/previews/6477b8546c9b62d74f338f9e/download/image.png"
-            alt="Personaje Simpsons"
-          />
-        </Col>
-        <Col md={8}>
-          <Card.Body className="w-100 pt-5">
-            <Card.Title className="pb-3">
-              <h2>Homer Simpson</h2>
-            </Card.Title>
-            <Card.Text>
-              Facts are meaningless. You could use facts to prove anything
-              that's even remotely true.
-            </Card.Text>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Card>
+    <Container>
+      <Card>
+        <Row>
+          <Col md={4}>
+            <img src={personaje.image} alt={personaje.character}></img>
+          </Col>
+          <Col md={8}>
+            <Card.Body>
+              <Card.Title>
+                <h2>{personaje.character}</h2>
+              </Card.Title>
+              <Card.Text>{personaje.quote}</Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
   );
 };
 
